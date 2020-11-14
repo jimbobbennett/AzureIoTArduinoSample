0dff4caae3b79ca88e5c58cadee36e65819f46ac
Jim Bennett <jim.bennett@microsoft.com>
2020-11-14 14:20:06 -0800

    Adding missing files

---
+++ src/iot_config.h
@@ -0,0 +1,6 @@
+#ifndef IOT_CONFIGS_H
+#define IOT_CONFIGS_H
+
+#define DEVICE_CONNECTION_STRING "<Your IoT Hub Device connection string>"
+
+#endif
---
+++ src/wifi_config.h
@@ -0,0 +1,7 @@
+#ifndef WIFI_CONFIG_H
+#define WIFI_CONFIG_H
+
+#define WIFI_SSID        "<Your WiFi SSID>"
+#define WIFI_PASSWORD    "<Your WiFi Password>"
+
+#endif
